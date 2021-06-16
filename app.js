48575b097c0c57de87ef23e880ed8393a6a95c20
jesse dills <jessedills@yahoo.com>
2021-06-16 18:11:52 -0400

    footer function and allstores function

--- app.js
+++ app.js
@@ -2,41 +2,6 @@
 
 const storeHours = ["6am", "7am", "8am", "9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm", "7pm"];
 
-// const seattle = new Sales("Seattle", 23, 65, 6.3);
-// const tokyo = new Sales("Tokyo", 3, 24, 1.2);
-// const dubai = new Sales("Dubai", 11, 38, 3.7);
-// const paris = new Sales("Paris", 20, 38, 2.3);
-// const lima = new Sales("Lima", 2, 16, 4.6,);
-// const storeArray = [seattle, tokyo, dubai, paris, lima];
-
-
-// const Seattle = {
-  //   customers: 0,
-  //   minCust: 23,
-  //   maxCust: 65,
-  //   hrSales: [],
-  //   avgCookiePerSale: 6.3,
-  //   cookiesPerHour: 0,
-  //   customerPerHr: function () {
-  //     this.customers = custPerHr(this.minCust, this.maxCust);
-  //   },
-  //   averageCookiePerHr: function () {
-  //     this.cookiesPerHour = avgCookiePerHr(this.avgCookiePerSale, this.customers);
-  //   },
-  //   salesByTheHour: function () {
-  //     for (let i = 0; i < storeHours.length; i++) {
-  //       this.customerPerHr();
-  //       this.averageCookiePerHr();
-  //       this.hrSales.push(this.cookiesPerHour);
-  //     }
-  //   },
-  // }
-  // Seattle.salesByTheHour();
-  // console.log(Seattle);
-        
-          
-const dailySalesDivElem = document.getElementById("daily sales");
-          
 // -----------------Constructor Function------------------------ //
           
 function Sales(locationName, minCust, maxCust, avgCookiePerSale) {
@@ -44,51 +9,45 @@ function Sales(locationName, minCust, maxCust, avgCookiePerSale) {
   this.minCust = minCust;
   this.maxCust = maxCust;
   this.avgCookiePerSale = avgCookiePerSale;
-  this.hrSales = [];
+  this.hourlySales = [];
   this.salesArray.push(this);
   console.log("this", this);
 }
-//---------------------------------------------------------------------------------//
+//-------------------Constructor End---------------------------------------------//
+//-------"Master Array"-----------//
 Sales.prototype.salesArray = [];
 
-// Sales.prototype.getCookieSales = function() {
-//   for (let i = 0; i < storeHours.length; i++) {
-//     const randomCust = Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);
-//     const cookieSales = (randomCust * Math.floor(this.avgCookiePerSale));
-//     this.hrSales.push(cookieSales);
-//   }
-// }
 
 Sales.prototype.custPerHr = function() {
   this.customers = Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);
 }
 
 Sales.prototype.avgCookiePerHr = function() {
-  this.cookiesPerHour = Math.floor(this.avgCookiePerSale * this.customers);
+  this.cookiesPerHr = Math.floor(this.avgCookiePerSale * this.customers);
 }
 
 Sales.prototype.salesByTheHour = function() {
   for (let i = 0; i < storeHours.length; i++) {
     this.custPerHr();
     this.avgCookiePerHr();
-    this.hrSales.push(this.cookiesPerHour);
+    this.hourlySales.push(this.cookiesPerHr);
   }
 }
 
+const dailysalesDivElem = document.getElementById("daily sales");
+// document.getElementById("daily sales")
+const tableElem = document.createElement("table");
+dailysalesDivElem.appendChild(tableElem);
 
-// // Sales.prototype.renderSales = function() {
-  
-// }
 
-document.getElementById("daily sales")
-  const tableElem = document.createElement("table");
-  dailySalesDivElem.appendChild(tableElem);
-
-function renderHours() {
+function renderHeader() {
   const tableHeader = document.createElement("thead");
   tableElem.appendChild(tableHeader);
   const row1 = document.createElement("tr");
   tableHeader.appendChild(row1);
+  const locationCell = document.createElement("th");
+  locationCell.textContent = ("Location");
+  row1.appendChild(locationCell);
 
   for (let i = 0; i < storeHours.length; i++) {
     const headerCell = document.createElement("th");
@@ -98,17 +57,23 @@ function renderHours() {
 }
   
 Sales.prototype.renderData = function() {
-  const storeRow = document.createElement("tr");
-  tableElem.appendChild(storeRow);
-  const storeColumn = document.createElement("col");
-  storeColumn.textContent = this.locationName;
-  storeRow.appendChild(storeColumn);
-
+  const locationRow = document.createElement("tr");
+  tableElem.appendChild(locationRow);
+  const locationCell = document.createElement("th");
+  locationCell.textContent = this.locationName;
+  locationRow.appendChild(locationCell);
+  
+  let dailyTotal = 0;
+  
   for (let i = 0; i < storeHours.length; i++) {
+    dailyTotal += this.hourlySales[i];
     const dataCell = document.createElement("td");
-    dataCell.textContent = this.hrSales[i];
-    storeRow.appendChild(dataCell);
+    dataCell.textContent = this.hourlySales[i];
+    locationRow.appendChild(dataCell);
   }
+  let dailyTotalSales = document.createElement("th");
+  dailyTotalSales.textContent = dailyTotal;
+  locationRow.appendChild(dailyTotalSales);
 }
   
 function renderAllStores() {
@@ -117,7 +82,6 @@ function renderAllStores() {
     currentStore.custPerHr();
     currentStore.avgCookiePerHr();
     currentStore.salesByTheHour();
-    // currentStore.getCookieSales();
     currentStore.renderData();
   }
 }
@@ -127,11 +91,33 @@ const tokyo = new Sales("Tokyo", 3, 24, 1.2);
 const dubai = new Sales("Dubai", 11, 38, 3.7);
 const paris = new Sales("Paris", 20, 38, 2.3);
 const lima = new Sales("Lima", 2, 16, 4.6,);
-// const storeArray = [seattle, tokyo, dubai, paris, lima];
+const storeArray = [seattle, tokyo, dubai, paris, lima];
+
+function renderFooter() {
+  const tableFooter = document.createElement("tfoot");
+  tableElem.appendChild(tableFooter);
+  const totalCell = document.createElement("th");
+  totalCell.textContent = ("Hourly Totals");
+  tableFooter.appendChild(totalCell);
+
+  let grandTotal = 0;
+
+  for (let i = 0; i < storeHours.length; i++) {
+    let hourlyTotal = 0
+    for (let j = 0; j < Sales.prototype.salesArray.length; j++) {
+      hourlyTotal += Sales.prototype.salesArray[j].hourlySales[i];
+    }
+    let storeHourTotal = document.createElement("th");
+    storeHourTotal.textContent = hourlyTotal;
+    tableFooter.appendChild(storeHourTotal);
+    grandTotal += hourlyTotal;
+  }
+  let grandTotalSales = document.createElement("th");
+  grandTotalSales.textContent = grandTotal;
+  tableFooter.appendChild(grandTotalSales);
+}
 
 
-renderHours();
+renderHeader();
 renderAllStores();
-  // Sales.prototype.renderSales();
-  
-  
+renderFooter();
